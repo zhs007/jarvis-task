bd6264c0f5624d5d5467081398062fad01d950ed
zhs007 <sssxueren@gmail.com>
2017-12-08 10:20:33 +0800

    add TaskChain & TaskFactory

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const { Task } = require('./lib/task');
+const { TaskChain } = require('./lib/taskchain');
+const { TaskFactory } = require('./lib/taskfactory');
 const { generateUUID } = require('./lib/utils');
 
 exports.Task = Task;
+exports.TaskChain = TaskChain;
+exports.TaskFactory = TaskFactory;
 
 exports.generateUUID = generateUUID;
