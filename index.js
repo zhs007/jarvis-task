ba69a0978ff47dcb6e3d3e76b4fcc8e60cc58407
zhs007 <sssxueren@gmail.com>
2017-12-09 17:13:35 +0800

    add TaskDeamon

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 const { Task } = require('./lib/task');
 const { TaskChain } = require('./lib/taskchain');
+const { TaskDeamon } = require('./lib/taskdeamon');
 const { TaskFactory } = require('./lib/taskfactory');
 const { generateUUID } = require('./lib/utils');
 
 exports.Task = Task;
 exports.TaskChain = TaskChain;
 exports.TaskFactory = TaskFactory;
+exports.TaskDeamon = TaskDeamon;
 
 exports.generateUUID = generateUUID;
