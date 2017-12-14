1d1ae51f4a5731958129b502bcb7220c9543e7d9
zhs007 <sssxueren@gmail.com>
2017-12-14 22:02:09 +0800

    add startTaskChain

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { TaskChain } = require('./lib/taskchain');
 const { TaskDeamon } = require('./lib/taskdeamon');
 const { TaskFactory } = require('./lib/taskfactory');
 const { generateUUID, hash_md5 } = require('./lib/utils');
+const { startTaskChain } = require('./lib/startfunc');
 
 exports.Task = Task;
 exports.TaskChain = TaskChain;
@@ -13,6 +14,8 @@ exports.TaskDeamon = TaskDeamon;
 
 exports.generateUUID = generateUUID;
 exports.hash_md5 = hash_md5;
+
+exports.startTaskChain = startTaskChain;
 
 const { JARVISTASK_NAMEID_INITCRAWLERMGR } = require('./lib/taskdef');
 const { regTaskFactory_InitCrawlerMgr } = require('./lib/task_initcrawlermgr');
