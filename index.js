54f2ccee61755166d427b9292eab10ec910ded31
zhs007 <sssxueren@gmail.com>
2017-12-14 19:53:55 +0800

    add crawlercore

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ exports.TaskFactory = TaskFactory;
 exports.TaskDeamon = TaskDeamon;
 
 exports.generateUUID = generateUUID;
+
+const { JARVISTASK_NAMEID_INITCRAWLERMGR } = require('./lib/taskdef');
+const { regTaskFactory_InitCrawlerMgr } = require('./lib/task_initcrawlermgr');
+
+exports.JARVISTASK_NAMEID_INITCRAWLERMGR = JARVISTASK_NAMEID_INITCRAWLERMGR;
+exports.regTaskFactory_InitCrawlerMgr = regTaskFactory_InitCrawlerMgr;
+
+const crawlercore = require('crawlercore');
+exports.crawlercore = crawlercore;
